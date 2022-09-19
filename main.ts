b824b3e6a114ee1d8c477700635d9dad98b383d4
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-19 10:34:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    number = randint(0, 2)
+    if (number == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else if (number == 1) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
+let number = 0
+basic.showIcon(IconNames.Angry)
